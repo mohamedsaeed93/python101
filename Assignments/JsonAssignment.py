af5cb3b671a289289c57534a57d0785ee95cbe8b
mohamedsaeed93 <mohamed.elsaeed93@gmail.com>
2016-03-17 16:45:31 +0200

    Adding instances of class as children to an object

--- Assignments/JsonAssignment.py
+++ Assignments/JsonAssignment.py
@@ -7,12 +7,24 @@ class JsonAssignment:
         self.attribute2 = 2
         self.attribute3 = {'key1': "value", 'key2': 1}
         self.attribute4 = [1, 2, 3, 4]
+        self.children = []
 
     def __str__(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
+    def set_children(self, children):
+        self.children = children
+
 
 class_object = JsonAssignment()
 print(class_object)
+print("--------------------------------------")
 class_object.attribute5 = (1, 2, 3, 4, 5)
 print(class_object)
+print("--------------------------------------")
+class_object2 = JsonAssignment()
+class_object3 = JsonAssignment()
+object_children = [class_object2, class_object3]
+class_object.set_children(object_children)
+print(class_object)
+print("--------------------------------------")
