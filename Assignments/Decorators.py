008239379ac93a983400964b609e83ad20d21e24
mohamedsaeed93 <mohamed.elsaeed93@gmail.com>
2016-03-24 14:02:23 +0200

    fixing all warnings in files

--- Assignments/Decorators.py
+++ Assignments/Decorators.py
@@ -5,18 +5,26 @@ class Decorator:
     def __call__(self, value, *args, **kwargs):
         def hello():
             return "Hello " + kwargs['name']
-        def sum():
-            sum = 0
+
+        def calculate_total():
+            total = 0
             for i in args:
-                sum+= i
-            return sum 
-        # maybe do something before the func call
-        response = self.func(value,*args, **kwargs)
+                total += i
+            return total
+            # maybe do something before the func call
+
+        response = self.func(value, *args, **kwargs)
         # maybe do something after the func call
-        return hello() + ", Sum = " + str(sum()) + ", Return Value of Function Call = " + response + ", Value = " + value 
+        return hello() + ", Sum = " + str(
+            calculate_total()) + ", Return Value of Function Call = " + response + ", Value = " + value
+
 
 @Decorator
-def test(value,*args, **kwargs): return str(value+"_updated")
+def test(value, *args, **kwargs):
+    print(args)
+    print(kwargs)
+    return str(value + "_updated")
 
-print(test("value",1,2,3,4, name = 'mohamed'))
+
+print(test("value", 1, 2, 3, 4, name='mohamed'))
 print(test.func("value"))
--- Small App/app.py
+++ Small App/app.py
@@ -1,52 +1,58 @@
 from flask import Flask, request, render_template, jsonify
 import os
 import json
-app = Flask(__name__)
-indir = 'Users/'
 
-#view all users
+app = Flask(__name__)
+directory = 'Users/'
+
+
+# view all users
 @app.route('/users/')
 def index():
     files = list()
-    for root, dirs, filenames in os.walk(indir):
-        for f in filenames:
-            files.append(json.load(open(indir + f)))
-    return jsonify(users = files)
+    for root, dirs, file_names in os.walk(directory):
+        for f in file_names:
+            files.append(json.load(open(directory + f)))
+    return jsonify(users=files)
 
-#show user based on username
+
+# show user based on username
 @app.route('/users/<username>')
 def show(username):
-    file = indir + username +'.json'
-    if(os.path.exists(file)): 
-       with open(file) as obj:
+    filename = directory + username + '.json'
+    if os.path.exists(filename):
+        with open(filename) as obj:
             return jsonify(json.load(obj))
     else:
         return "User not found!"
 
-#create user
+
+# create user
 @app.route('/users/', methods=['POST'])
 def create():
-    username = request.form.get("username",str)
-    email = request.form.get("email",str)
-    age = request.form.get("age",int)
-    file = indir + username + '.json'
-    data = { 'username' : username, 'email' : email, 'age' : age }
-    if(not os.path.exists(file)): 
-        with open(file, 'w') as obj:
-            json.dump(data,obj)
+    username = request.form.get("username", str)
+    email = request.form.get("email", str)
+    age = request.form.get("age", int)
+    filename = directory + username + '.json'
+    data = {'username': username, 'email': email, 'age': age}
+    if not os.path.exists(filename):
+        with open(filename, 'w') as obj:
+            json.dump(data, obj)
             return "User created successfully"
     return "User already exists!"
 
-#delete user
-@app.route('/users/', methods =['DELETE'])
+
+# delete user
+@app.route('/users/', methods=['DELETE'])
 def delete():
-    username = request.form.get("username",str)
-    file = indir + username +".json"
-    if(os.path.exists(file)): 
-        os.remove(file)
+    username = request.form.get("username", str)
+    filename = directory + username + ".json"
+    if os.path.exists(filename):
+        os.remove(filename)
         return "User deleted successfully"
     else:
         return "User is not found"
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0', debug=True)
